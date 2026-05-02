8e680e823a915b893e19858b45ee945ab7b2bcd6
Jatinshiyani8141 <jatinshiyani81414@gmail.com>
2026-05-01 21:30:17 -0400

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+body {
+    margin: 0;
+    font - family: Arial, sans - serif;
+    background: linear - gradient(120deg, #1e3c72, #2a5298);
+    color: white;
+    text - align: center;
+}
+
+.title {
+    margin - top: 20px;
+    font - size: 32px;
+}
+
+.video - container {
+    margin: 30px auto;
+}
+
+#box {
+    width: 50px;
+    height: 50px;
+    background - color: yellow;
+    position: relative;
+    margin: 20px auto;
+    border - radius: 10px;
+}
