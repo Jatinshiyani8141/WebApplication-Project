d6ff51afc65345a9cf518f3d207afd8b51409853
Jatinshiyani8141 <jatinshiyani81414@gmail.com>
2026-06-28 17:52:11 -0400

    Completed operator overloading assignment

---
+++ OperatorOverloadingAssignment/OperatorOverloadingAssignment/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace OperatorOverloadingAssignment
+{
+    // Create the Employee class
+    public class Employee
+    {
+        // Employee ID
+        public int Id { get; set; }
+
+        // Employee first name
+        public string FirstName { get; set; }
+
+        // Employee last name
+        public string LastName { get; set; }
+
+        // Compare two Employee objects using their Id
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+
+        // Required when overloading ==
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+
+        // Override Equals
+        public override bool Equals(object obj)
+        {
+            Employee employee = obj as Employee;
+
+            if (employee == null)
+            {
+                return false;
+            }
+
+            return Id == employee.Id;
+        }
+
+        // Override GetHashCode
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ OperatorOverloadingAssignment/OperatorOverloadingAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OperatorOverloadingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the first employee
+            Employee employee1 = new Employee()
+            {
+                Id = 1,
+                FirstName = "Jatin",
+                LastName = "Shiyani"
+            };
+
+            // Create the second employee
+            Employee employee2 = new Employee()
+            {
+                Id = 2,
+                FirstName = "Jason",
+                LastName = "Russel"
+            };
+
+            // Compare the employees
+            Console.WriteLine(employee1 == employee2);
+
+            Console.ReadLine();
+        }
+    }
+}
